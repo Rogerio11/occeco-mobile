414c0629af862472e024dff0ecfe9ca555ca8a36
Alexiaognard <ognardalexiats4@gmail.com>
2021-04-15 10:52:13 +0200

    ADD : BottomTabNavigator

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -6,6 +6,7 @@ import SignUpScreen from './User/SignUp';
 import ProfileScreen from './User/Profile';
 import ArticleScreen from './Article/Article';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
 
@@ -32,9 +33,30 @@ const Tab = createBottomTabNavigator();
 
 const Navigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Articles" component={ArticleStackNavigator} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Mon Compte') {
+            iconName = 'ios-person';
+          } else if (route.name === 'Articles') {
+            iconName = 'ios-list';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+
+      })}
+      tabBarOptions={{
+        activeTintColor: 'white',
+        inactiveTintColor: 'black',
+        activeBackgroundColor: "green",
+      }}
+    >
       <Tab.Screen name="Mon Compte" component={ProfileStackNavigator} />
+      <Tab.Screen name="Articles" component={ArticleStackNavigator} />
     </Tab.Navigator>
   );
 };
