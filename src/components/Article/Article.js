ced837bd1c9e152f2ef4cec5daf476ba175908ae
Alexiaognard <ognardalexiats4@gmail.com>
2021-05-03 14:07:02 +0200

    ADD : Article Detailed

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,24 +1,60 @@
 import React, { useState } from 'react';
-import { Button, Input} from 'react-native-elements';
-import {useDispatch} from "react-redux";
+import { Card, Text, FAB, Divider} from 'react-native-elements';
+import {useDispatch, useSelector} from "react-redux";
 import { View} from 'react-native';
+import DeleteArticle from "./DeleteArticle";
+import moment from 'moment';
 
+moment.updateLocale('fr');
 const ArticleScreen = ({ navigation, route}) => {
-    console.log("////////////params/////////")
-    console.log(route.params)
-    console.log("/////////////////////")
-    const article = route.params.article;
     
-
-
+    const article = route.params.article;
+    const user = useSelector(state => state.User)
+    const [deleteArticle, setDeleteArticle] = useState(false);
+    const [idArticleDelete, setIdArticleDelete] = useState(null);
+    const handleDelete = (id) => {
+        setIdArticleDelete(id)
+        setDeleteArticle(true)
+      }
     return (
-        <View>
-
-            <Text>{article.articleTitle}</Text>
-            <Text>{article.articleDescription}</Text>
-            <Text>{article.articleLink}</Text>
+        <View style={{ alignItems:'center' }}>
+            <Card containerStyle={{height:'100%', width:'100%'}}>
             
+            <Text h3>{article.articleTitle}</Text>
+            <Divider />
+            <Text>{moment(article.articleStartDate).format('DD/MM/YYYY')} - {moment(article.articleEndDate).format('DD/MM/YYYY')}</Text>
+            <Text>Description : {article.articleDescription}</Text>
+            <Text>Lien : {article.articleLink}</Text>
+            
+            {deleteArticle &&  <DeleteArticle handleClose={() => setDeleteArticle(false)} idArticle={idArticleDelete}/> }
 
+            {
+              user && user.user && (user.user.accountType === "admin" || user.user.accountType === "partner") &&
+
+              <View style={{ flexDirection: "row", justifyContent:'center' }}>
+                  <FAB
+                        visible={true}
+                        icon={{ name: 'content-copy', color: 'white' }}
+                        buttonStyle={{backgroundColor:'green'}}
+                        onPress={() => navigation.push('Duplicate Article', {article: article})}
+                  />
+                  <Text> </Text>
+                  <FAB
+                      visible={true}
+                      icon={{ name: 'edit', color: 'white' }}
+                      buttonStyle={{backgroundColor:'gray'}}
+                      onPress={() => navigation.push('Update Article', {article: article})}
+                  />
+                  <Text> </Text>
+                  <FAB
+                      visible={true}
+                      icon={{ name: 'delete', color: 'white' }}
+                      buttonStyle={{backgroundColor:'red'}}
+                      onPress={() => handleDelete(article._id)}
+                  />
+                  </View>
+            }
+            </Card>
         </View>
     );
 };
--- src/components/Article/ListArticle.js
+++ src/components/Article/ListArticle.js
@@ -1,31 +1,24 @@
 import React, {useState} from 'react';
 
-import { View, FlatList } from 'react-native';
+import { View, FlatList, TouchableHighlight } from 'react-native';
 import {useDispatch, useSelector} from "react-redux";
 import {getAllArticles} from "../../actions/ArticleActions";
-import DeleteArticle from "./DeleteArticle"
-import { Text, Button, } from 'react-native-elements';
+import { Text, Icon, Card, SpeedDial, useTheme } from 'react-native-elements';
 
 
 function ListArticleScreen({ navigation }) {
   const user = useSelector(state => state.User)
   const dispatch = useDispatch();
   const list = useSelector(state => state.Article);
-  // const [addType, setAddType] = useState(false);
   const [listArticle, setListArticle] = useState(Array.isArray(list.articles) ? list.articles : []);
-  const [deleteArticle, setDeleteArticle] = useState(false);
-  const [idArticleDelete, setIdArticleDelete] = useState(null);
-  // const [editType, setEditType] = useState(false);
+  const [open, setOpen] = useState(false);
+  const { theme } = useTheme();
   if (listArticle.length && listArticle.length === 0 ){
     dispatch(getAllArticles());
     setListArticle(useSelector(state => state.articles))
   }
 
-  
-  const handleDelete = (id) => {
-    setIdArticleDelete(id)
-    setDeleteArticle(true)
-  }
+
   const DATA = [
     {
       id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
@@ -43,54 +36,61 @@ function ListArticleScreen({ navigation }) {
   return (
     
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
-      <FlatList
-        data={DATA}
-        keyExtractor={(item) => item._id}
-        renderItem={({item}) => 
-        <View>
-            <Text h4>{item.articleTitle}</Text>
-            
-            <View style={{ flexDirection: "row" }}>
-            <Button
-                title="Duplicate"
-                onPress={() => navigation.push('Duplicate Article', {article: item})}
-            />
-            <Text> </Text>
-            <Button
-                title="Update"
-                onPress={() => navigation.push('Update Article', {article: item})}
-            />
-            <Text> </Text>
-            <Button
-                title="Delete"
-                onPress={() => handleDelete(item._id)}
-            />
+      <Card containerStyle={{width: '100%', height:'100%'}}>
+        {
+          listArticle.length === 0
+          ? <View style={{width: '100%', height:'100%'}}>
+              <Text>Pas d'articles à afficher</Text>
             </View>
-        </View>}
-      />
-
-      {deleteArticle &&  <DeleteArticle handleClose={() => setDeleteArticle(false)} idArticle={idArticleDelete}/> }
-
+          :
+          <View style={{width: '100%', height:'97%'}}>
+            <FlatList
+              data={listArticle}
+              keyExtractor={(item) => item._id}
+              renderItem={({item, index, separators}) => 
+              <TouchableHighlight
+                key={item._id}
+                onPress={() => navigation.push('Article', {article: item})}
+                onShowUnderlay={separators.highlight}
+                onHideUnderlay={separators.unhighlight}
+                >
+                <View style={{ flexDirection: "row", justifyContent:'space-between', backgroundColor:(index%2 === 0 ? theme.colors.secondary : 'white')}}>
+                    <Text h4>{item.articleTitle}</Text>
+                    <Icon name="chevron-right" type="material-community" onPress={() => navigation.push('Article', {article: item})}/>
+                </View>
+              </TouchableHighlight>
+              }
+            />
+          </View>
+      }
       {
         user && user.user && (user.user.accountType === "admin" || user.user.accountType === "partner") &&
-
-        <View style={{ flexDirection: "row" }}>
-
-          <Button
-          title="add Article"
-          onPress={() => navigation.push('Add Article')}
-        />
-        <Text> </Text>
-        
-        <Button
-          title="Voir Catégories"
-          onPress={() => navigation.push('Catégories')}
-        />
+        <View style={{width: '100%', height:'3%'}}>
+          <SpeedDial
+            isOpen={open}
+            icon={{ name: 'edit', color: 'white' }}
+            openIcon={{ name: 'close', color: 'white' }}
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            buttonStyle={{backgroundColor:'green', position:'relative'}}
+            
+          >
+            <SpeedDial.Action
+              icon={{ name: 'add', color: 'white' }}
+              title="Nouvel article"
+              onPress={() => navigation.push('Add Article')}
+              buttonStyle={{backgroundColor:'green'}}
+            />
+            <SpeedDial.Action
+              icon={{ name: 'visibility', color: 'white' }}
+              title="Voir catégories"
+              onPress={() => navigation.push('Catégories')}
+              buttonStyle={{backgroundColor:'green'}}
+            />
+          </SpeedDial>
         </View>
       }
-      
-      
+      </Card>
     </View>
     
   );
